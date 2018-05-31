67bbdaa1a2de4a6e3be4f8b196445af75216c469
minsik moon <msmoon.business@gmail.com>
2018-06-01 01:07:48 +0900

    kakao keyboard api추가, json-simple 추가

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -5,11 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-	
+
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
 		return "welcome";
 	}
 
-	
+	// 방참여 매핑 : chat guest 전용 //host와 client 분기시켜야 한다.
+	@ResponseBody
+	@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
+	public String keyboard() {
+		System.out.println("keyboard 시작됨");
+
+		JSONObject jobjBtn = new JSONObject();
+		ArrayList<String> btns = new ArrayList<String>();
+		btns.add("신청1");
+		btns.add("신청2 테스틓ㅎ");
+
+		jobjBtn.put("type", "buttons");
+		jobjBtn.put("buttons", btns);
+		System.out.println(jobjBtn.toJSONString());
+		return jobjBtn.toJSONString();
+	}
+
 }
