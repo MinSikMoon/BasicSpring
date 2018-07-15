ee2c53c1a08e57c4ee9c7fcffa5bf38d7a5b2115
MinSikMoon <msmoon.business@gmail.com>
2018-07-15 21:02:55 +0900

    log4j 실습

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -1,13 +1,16 @@
 package controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-	
+	private static final Logger logger = Logger.getLogger(WelcomeController.class);
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
+		if(logger.isDebugEnabled())
+			logger.debug("welcomecontroller 동작함.");
 		return "welcome";
 	}
 
